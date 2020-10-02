1bf1408966765c4efb0a23d362e6ada9c108cea6
XiN <ranveermedran@gmail.com>
2020-10-02 17:30:19 +0530

    firebase connection

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,23 +1,30 @@
-// ADD_EXPENSE
 import {v4 as uuidv4} from "uuid";
+import database from '../firebase/firebase'
 
-export const addExpense = (
-    {
-        description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
-    } = {}
-) => ({
+// ADD_EXPENSE
+export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
-    expense: {
-        id: uuidv4(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
+    expense
 });
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch) => {
+        const {
+            description = '',
+            note = '',
+            amount = 0,
+            createdAt = 0
+        } = expenseData
+        const expense = { description, note, amount, createdAt }
+
+       return database.ref('expenses').push(expense).then((ref) => {
+           dispatch(addExpense({
+               id: ref.key,
+               ...expense
+           }))
+       })
+    }
+}
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
@@ -31,3 +38,27 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = []
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                })
+            })
+
+            dispatch(setExpenses(expenses))
+        })
+
+    }
+}
---
+++ src/firebase/firebase.js
@@ -0,0 +1,103 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7fWgmneah6KdkftBMwcMNekrpQEoTNK4",
+    authDomain: "expensify-app-2d1db.firebaseapp.com",
+    databaseURL: "https://expensify-app-2d1db.firebaseio.com",
+    projectId: "expensify-app-2d1db",
+    storageBucket: "expensify-app-2d1db.appspot.com",
+    messagingSenderId: "343342621126",
+    appId: "1:343342621126:web:0126df35a954aac9d492c7",
+    measurementId: "G-65JDZN4XH0"
+};
+
+firebase.initializeApp(firebaseConfig)
+const database = firebase.database()
+
+export { firebase, database as default }
+
+// child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+//
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = []
+//
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         })
+//     })
+//
+//     console.log(expenses)
+// })
+
+// database.ref('expenses').push({
+//     description: 'flutter',
+//     note: 'cross',
+//     amount: 20,
+//     createdAt: '20/2/20'
+// })
+// database.ref('expenses').push({
+//     description: 'native',
+//     note: 'cross v',
+//     amount: 2,
+//     createdAt: '2/2/20'
+// })
+// database.ref('expenses').push({
+//     description: 'dart',
+//     note: 'crosss',
+//     amount: 200,
+//     createdAt: '20/3/20'
+// })
+
+// database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val().name + ' is a Software developer at Amazon')
+// })
+
+// database.ref().set({
+//     name: 'Ranveer Oshan',
+//     age: 26,
+//     isSingle: false,
+//     location: {
+//         city: 'Duliajan',
+//         country: 'India'
+//     }
+// }).then(() => {
+//     console.log('Saved')
+// }).catch((e) => {
+//     console.log('Failed', e)
+// })
+//
+// database.ref().update({
+//     'job/title': 'Manager',
+//     'location/city': 'Mirza'
+// })
+//
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// })
+//
+//
+//
+// database.ref('age').set(27)
+// database.ref('location/city').set('Guwahati')
+//
+// database.ref('attributes/height').set('60 kg')
+// database.ref('attributes/weight').set('170 cm')
+//
+// database.ref('isSingle').remove().then(() => {
+//     console.log('Removed')
+// }).catch((e) => {
+//     console.log('Failed', e)
+// })
+
